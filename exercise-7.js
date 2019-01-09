c1e57463b030921fe8c51c94ac9326698912951d
restaadiputra <resta.adiputra@gmail.com>
2019-01-09 15:50:05 +0800

    add recursive to exercise.7

--- exercise-7.js
+++ exercise-7.js
@@ -8,3 +8,23 @@ console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
 console.log(hitungJumlahKata('A song to sing')); // 4
 console.log(hitungJumlahKata('I')); // 1
 console.log(hitungJumlahKata('I believe I can code')); // 5
+
+
+function hitungJumlahKata2(kalimat) {
+  var newKalimat = kalimat.split(' ')
+
+  if(newKalimat.length === 1) {
+    return 1
+  } else if(newKalimat.length === 0) {
+    return 0
+  } else{
+    return 1 + hitungJumlahKata2(newKalimat.slice(1).join(' '))
+  }
+}
+
+// TEST CASES
+console.log(hitungJumlahKata2('I have a dream')); // 4
+console.log(hitungJumlahKata2('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata2('A song to sing')); // 4
+console.log(hitungJumlahKata2('I')); // 1
+console.log(hitungJumlahKata2('I believe I can code')); // 5
